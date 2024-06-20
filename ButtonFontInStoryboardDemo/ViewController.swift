f0f6b604d70eb44d99b5c203b0dfa1401b31b6df
Matt Neuburg <matt@tidbits.com>
2024-06-20 08:25:36 -0700

    show how to change title

--- ButtonFontInStoryboardDemo/ViewController.swift
+++ ButtonFontInStoryboardDemo/ViewController.swift
@@ -1,4 +1,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func doButton (_ sender: UIButton) {
+        sender.replaceTitle("Hello There!")
+    }
 }
+
+extension UIButton {
+    func replaceTitle(_ newTitle: String) {
+        guard configuration != nil else {
+            setTitle(newTitle, for: .normal)
+            return
+        }
+        guard configuration?.attributedTitle != nil else {
+            configuration?.title = newTitle
+            return
+        }
+        configuration?.attributedTitle?.characters = .init(newTitle)
+    }
+}
+
